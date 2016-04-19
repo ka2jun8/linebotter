67f352879fafac665bf50229ab995780df2d239a
ka2jun8 <narchbald103@gmail.com>
2016-04-20 08:40:29 +0900

    treat a default page

--- index.js
+++ index.js
@@ -17,8 +17,170 @@ app.get('/', function(req, res) {
 });
 app.post('/', function(req, res) {
     console.log("kani::: "+JSON.stringify(req.body));
-    res.send('Hello World!');
+
+    async.waterfall([
+        // ぐるなびAPI
+        function(callback) {
+
+            var json = req.body;
+
+            console.log("kani::: "+JSON.stringify(json));
+
+            // 受信テキスト
+            var search_place = json['result'][0]['content']['text'];
+            var search_place_array = search_place.split("\n");
+
+            //検索キーワード
+            var gnavi_keyword = "";
+            if(search_place_array.length == 2){
+                var keyword_array = search_place_array[1].split("、");
+                gnavi_keyword = keyword_array.join();
+            }
+
+            console.log("kani::: place="+search_place+"/key="+gnavi_keyword);
+
+            // ぐるなびAPI レストラン検索API
+            var gnavi_url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';
+
+            console.log("gnavi = "+process.env.GNAVI_KEY);
+
+            // ぐるなび リクエストパラメータの設定
+            var gnavi_query = {
+                "keyid": process.env.GNAVI_KEY,
+                "format": "json",
+                "address": search_place_array[0],
+                "hit_per_page": 1,
+                "freeword": gnavi_keyword,
+                "freeword_condition": 2
+            };
+            var gnavi_options = {
+                url: gnavi_url,
+                headers : {'Content-Type' : 'application/json; charset=UTF-8'},
+                qs: gnavi_query,
+                json: true
+            };
+
+            // 検索結果をオブジェクト化
+            var search_result = {};
+
+            request.get(gnavi_options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    if('error' in body){
+                        console.log("検索エラー" + JSON.stringify(body));
+                        return;
+                    }
+
+                    // 店名
+                    if('name' in body.rest){
+                        search_result['name'] = body.rest.name;
+                    }
+                    // 画像
+                    if('image_url' in body.rest){
+                        search_result['shop_image1'] = body.rest.image_url.shop_image1;
+                    }
+                    // 住所
+                    if('address' in body.rest){
+                        search_result['address'] = body.rest.address;
+                    }
+                    // 緯度
+                    if('latitude' in body.rest){
+                        search_result['latitude'] = body.rest.latitude;
+                    }
+                    // 軽度
+                    if('longitude' in body.rest){
+                        search_result['longitude'] = body.rest.longitude;
+                    }
+                    // 営業時間
+                    if('opentime' in body.rest){
+                        search_result['opentime'] = body.rest.opentime;
+                    }
+
+                    console.log("kani::: "+JSON.stringify(search_result));
+
+                    callback(null, json, search_result);
+
+                } else {
+                    console.log('error: '+ response.statusCode);
+                }
+            });
+
+        },
+    ],
+
+    // LINE BOT
+    function(err, json, search_result) {
+                
+        if(err){
+            return;
+        }
+
+        //ヘッダーを定義
+        var headers = {
+            'Content-Type' : 'application/json; charset=UTF-8',
+            'X-Line-ChannelID' : process.env.LINE_CHANNELID, 
+            'X-Line-ChannelSecret' : process.env.LINE_SECRET, 
+            'X-Line-Trusted-User-With-ACL' : process.env.LINE_MID 
+        };
+
+        // 送信相手の設定（配列）
+        var to_array = [];
+        to_array.push(json['result'][0]['content']['from']);
+
+
+        // 送信データ作成
+        var data = {
+            'to': to_array,
+            'toChannel': 1383378250, //固定
+            'eventType':'140177271400161403', //固定
+            "content": {
+                "messageNotified": 0,
+                "messages": [
+                    // テキスト
+                    {
+                        "contentType": 1,
+                        "text": 'こちらはいかがですか？\n【お店】' + search_result['name'] + '\n【営業時間】' + search_result['opentime'],
+                    },
+                    // 画像
+                    {
+                        "contentType": 2,
+                        "originalContentUrl": search_result['shop_image1'],
+                        "previewImageUrl": search_result['shop_image1']
+                    },
+                    // 位置情報
+                    {
+                        "contentType":7,
+                        "text": search_result['name'],
+                        "location":{
+                            "title": search_result['address'],
+                            "latitude": Number(search_result['latitude']),
+                            "longitude": Number(search_result['longitude'])
+                        }
+                    }
+                ]
+            }
+        };
+        
+        console.log("kani::: data= "+ JSON.stringify(data));
+        console.log("proxy-url : "+process.env.FIXIE_URL);
+        
+        //オプションを定義
+        var options = {
+            url: 'https://trialbot-api.line.me/v1/events',
+            proxy : process.env.FIXIE_URL,
+            headers: headers,
+            json: true,
+            body: data
+        };
+
+        request.post(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            } else {
+                console.log('error: '+ JSON.stringify(response));
+            }
+        });
 });
+
 
 app.get('/callback', function(req, res) {
     console.log("kani::: "+JSON.stringify(req.body));
@@ -119,7 +281,6 @@ app.post('/callback', function(req, res){
     // LINE BOT
     function(err, json, search_result) {
                 
-        /*
         if(err){
             return;
         }
@@ -189,10 +350,8 @@ app.post('/callback', function(req, res){
                 console.log('error: '+ JSON.stringify(response));
             }
         });
-
-        */
         
-        res.send(search_result);
+        //res.send(search_result);
     });
 
 });
